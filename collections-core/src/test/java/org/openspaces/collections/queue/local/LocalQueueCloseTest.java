fd6d6194531a8671b66c4e9b165bf4fd0285b69c
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2016-01-04 17:45:44 +0200

    added close test for local queue

---
+++ collections-core/src/test/java/org/openspaces/collections/queue/local/LocalQueueCloseTest.java
@@ -0,0 +1,24 @@
+package org.openspaces.collections.queue.local;
+
+import org.junit.runner.RunWith;
+import org.openspaces.collections.GigaQueueConfigurer;
+import org.openspaces.collections.queue.GigaBlockingQueue;
+import org.openspaces.collections.queue.QueueCloseTest;
+import org.openspaces.collections.set.SerializableType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.openspaces.collections.CollocationMode.LOCAL;
+
+/**
+ * @author Oleksiy_Dyagilev
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class LocalQueueCloseTest extends QueueCloseTest{
+
+    @Override
+    public GigaBlockingQueue<SerializableType> createQueue() {
+        return new GigaQueueConfigurer<SerializableType>(gigaSpace, "TestClosingLocalGigaBlockingQueue", LOCAL).gigaQueue();
+    }
+}
