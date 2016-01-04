bcfbb2da2b7f0668597a69da3e02630dc865da58
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2016-01-04 15:41:40 +0200

    producer-consumer concurrent test

---
+++ collections-core/src/test/java/org/openspaces/collections/queue/concurrent/EmbeddedQueueProducerConsumerTest.java
@@ -0,0 +1,13 @@
+package org.openspaces.collections.queue.concurrent;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Oleksiy_Dyagilev
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/gigaqueue-embedded-test-context.xml")
+public class EmbeddedQueueProducerConsumerTest extends BaseProducerConsumerTest {
+}
